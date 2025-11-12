5ad3201fee17782526a8b581d5d54c07ccd4bc22
Carlos Torres de Freitas <ctfcarlostorres@hotmail.com>
2025-11-11 23:16:06 -0300

    Update installchrome.sh

--- installchrome.sh
+++ installchrome.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # instalar o google chrome
-wget -ct0 https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-chmod +x google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
+cd /tmp && wget -O google-chrome-stable.deb 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'
+sudo apt install ./google-chrome-stable.deb && cd $HOME
 
